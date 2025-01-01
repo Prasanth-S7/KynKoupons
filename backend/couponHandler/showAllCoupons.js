03eb4eee0931e687484bee06860ad25de0513c21
unknown <saivishwaram.ramkumar@gmail.com>
2025-01-01 23:07:01 +0530

    reference data forwarding is set

--- backend/couponHandler/showAllCoupons.js
+++ backend/couponHandler/showAllCoupons.js
@@ -3,6 +3,11 @@ const { fetchAll } = require("../db/basicCRUD/showall");
 const getAllCoupons = async(req,res) => {
   try {
     const result = await fetchAll("allCoupons");
+    result.data.forEach(val => {
+      // console.log(val["partnerInfo"]);
+      val["criteriaType"] = val["criteriaType"]["_path"]["segments"][1]; 
+      val["partnerInfo"] = val["partnerInfo"]["_path"]["segments"][1]; 
+    })
     res.status(200).json(result);
   } catch (error) {
     console.error("Error fetching data:", error);
